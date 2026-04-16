62b9b9a0f0a43954b41f9e2f361684c2a2321aba
Lukas Vitala <lukas.vitala@cz.stroeer-labs.com>
2026-04-16 09:50:19 +0200

    add test login account

--- server.ts
+++ server.ts
@@ -120,6 +120,7 @@ app.use(
 				defaultSrc: ["'self'"],
 				styleSrc: ["'self'", "'unsafe-inline'"],
 				scriptSrc: ["'self'", "'unsafe-inline'"],
+				scriptSrcAttr: ["'unsafe-inline'"],
 				imgSrc: ["'self'", "data:", "https:"],
 				connectSrc: ["'self'"],
 				fontSrc: ["'self'"],
